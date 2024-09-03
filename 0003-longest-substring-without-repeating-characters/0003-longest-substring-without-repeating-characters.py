4aaa4aede479281200855a67538938308254c7f6
Venky0508 <64658289+Venky0508@users.noreply.github.com>
2024-09-03 09:32:03 -0400

    Time: 281 ms (10.37%), Space: 16.5 MB (81.44%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,18 +1,47 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 0 or len(s) == 1:
-            return len(s)
+#         if len(s) == 0 or len(s) == 1:
+#             return len(s)
         
-        ans = 1
-        for i in range(len(s)):
-            j = i + 2
+#         ans = 1
+#         for i in range(len(s)):
+#             j = i + 2
             
-            while j <= len(s):
-                temp = list(s[i:j])
-                check = set(temp)
-                if len(temp) != len(check):
+#             while j <= len(s):
+#                 temp = list(s[i:j])
+#                 check = set(temp)
+#                 if len(temp) != len(check):
+#                     break
+#                 j += 1
+#                 if len(temp) > ans:
+#                     ans = len(temp)
+#         return ans
+
+        if s == "":
+            return 0
+
+        max_val = float("-inf")
+        temp_set = set()
+
+    
+
+        for i in range(len(s)):
+            temp_set.add(s[i])
+            for j in range (i+1, len(s)):
+                if s[j] not in temp_set:
+                    temp_set.add(s[j])
+                
+                else:
+                    n = len(temp_set)
+                    if max_val < n :
+                        max_val = n
+                    temp_set = set() 
                     break
-                j += 1
-                if len(temp) > ans:
-                    ans = len(temp)
-        return ans
+            
+            x = len(temp_set)
+            if x != 0 and max_val < x :
+                max_val = x
+
+
+
+        return int(max_val)
