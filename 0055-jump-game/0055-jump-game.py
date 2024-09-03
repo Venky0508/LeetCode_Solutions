57ddee873c048068f2209d3e847d9e0c91281193
Venky0508 <64658289+Venky0508@users.noreply.github.com>
2024-09-03 14:04:09 -0400

    Time: 4074 ms (8.18%), Space: 18.6 MB (6.32%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return True
+        goal = len(nums) - 1
+        
+        start = 0
+        stack = []
+        stack.append(0)
+        visited = set()
+        
+        while stack != []:
+            index = stack.pop()
+            visited.add(index)
+            val = nums[index]
+            while val > 0:
+                new = index + val
+                if new == goal:
+                    return True
+                if new not in visited:
+                    stack.append(new)
+                val -= 1
+            stack.sort()
+            while stack != [] and stack[-1] in visited:
+                stack.pop()
+                if stack == []:
+                    break
+        
+        return False
