7ab050e8dda4e4a45287853a6bc148af34876f1a
Venky0508 <64658289+Venky0508@users.noreply.github.com>
2025-04-13 22:45:31 -0700

    Time: 0 ms (100%), Space: 19 MB (34.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,25 +1,37 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        arr = list(nums)
-        arr.sort()
+        # arr = list(nums)
+        # arr.sort()
 
-        i = 0
-        j = len(arr) - 1
-        x = 0
-        y = 0
-        while i < j:
-            if arr[i] + arr[j] == target:
-                x = arr[i]
-                y = arr[j]
-                break
+        # i = 0
+        # j = len(arr) - 1
+        # x = 0
+        # y = 0
+        # while i < j:
+        #     if arr[i] + arr[j] == target:
+        #         x = arr[i]
+        #         y = arr[j]
+        #         break
 
-            elif arr[i] + arr[j] > target:
-                j -= 1
+        #     elif arr[i] + arr[j] > target:
+        #         j -= 1
 
-            else:
-                i += 1
+        #     else:
+        #         i += 1
 
-        a = nums.index(x)
-        nums[a] = float('inf')
-        b = nums.index(y)
-        return [a,b]
+        # a = nums.index(x)
+        # nums[a] = float('inf')
+        # b = nums.index(y)
+        # return [a,b]
+
+        numMap = dict()
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+
+            if diff in numMap:
+                return[numMap[diff],i]
+            
+            numMap[nums[i]] = i
+
+        
